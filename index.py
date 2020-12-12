2e45be72ab2f936fcd5ab8fa26b826e1c24e7159
Emmanuel <52857931+eeTiboah@users.noreply.github.com>
2020-12-12 17:53:53 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,36 @@
+#Hangman Project
+import random
+import hangman_art
+import hangman_words
+
+
+word_list=hangman_words.word_list
+chosen_word=random.choice(word_list)
+end_game=False
+lives=6
+word_length = len(chosen_word)
+word_replace=chosen_word.replace(chosen_word, "_"*word_length)
+word_replace_list=list(word_replace)
+print(hangman_art.logo)
+print(chosen_word)
+
+while not end_game:
+	guess_letter = input("Guess a letter: ").lower()
+	if guess_letter in word_replace_list:
+		print(f"You\'ve already guessed {guess_letter}")
+	for position in range(word_length):
+		letter=chosen_word[position]
+		if letter==guess_letter:
+			word_replace_list[position]=letter
+	if guess_letter not in chosen_word:
+		print(f"You guessed {guess_letter} which is not in the word. You lose a life")
+		lives-=1
+		if lives==0:
+			print("You lose!.")
+			end_game=True
+	print("  ".join(word_replace_list))
+	if "_" not in word_replace_list:
+		end_game=True
+		print("You win!.")
+	print(hangman_art.stages[lives])
+
